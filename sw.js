e82254e49aa959612c3c68979d17f211d9abf6cb
rams2state <ramstostate@gmail.com>
2026-07-17 16:29:58 -0500

    Deploy garden app - 2026-07-17 16:29

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Self-destructing SW -- clears old cache on every deploy
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.registration.unregister())
+  );
+});
